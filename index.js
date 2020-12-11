22b7e59ac6d661f28812790758064d3cfeb97daa
majerhua123 <majerhua123@gmail.com>
2020-12-11 09:57:47 -0500

    refactorizando listar suscripciones

--- index.js
+++ index.js
@@ -1,57 +1,60 @@
-'use strict'
+"use strict";
 //PARA PODER USAR LAS FUNCIONALIDADES DE ECMASCRIPT6
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 //setting
-app.set('view engine','ejs');
-app.set('views', path.join(__dirname,'views'));
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 //middleware
-app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-const validacionSuscriptorCtrl = require('./src/controllers/api_validacion_suscriptor');
-const listaSuscripcionesCtrl = require('./src/controllers/api_info_suscripciones');
-const paquetesCtrl = require('./src/controllers/api_info_paquete');
-const descuentoCtrl = require('./src/controllers/api_info_descuento');
-
-
-
+const validacionSuscriptorCtrl = require("./src/controllers/api_validacion_suscriptor");
+const listaSuscripcionesCtrl = require("./src/controllers/api_info_suscripciones");
+const paquetesCtrl = require("./src/controllers/api_info_paquete");
+const descuentoCtrl = require("./src/controllers/api_info_descuento");
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
 });
 
-
 /* RUTAS DE ACCESO A LOS SERVICIOS */
-app.post('/api/validarsuscriptor',validacionSuscriptorCtrl.getValidacionSuscriptor);
-app.post('/api/lista-suscripciones',listaSuscripcionesCtrl.getListaSuscripciones);
-app.post('/api/info-paquetes',paquetesCtrl.getPaquetes);
-app.post('/api/info-descuentos',descuentoCtrl.getDescuento);
+app.post(
+  "/api/validarsuscriptor",
+  validacionSuscriptorCtrl.getValidacionSuscriptor
+);
+app.post(
+  "/api/lista-suscripciones",
+  listaSuscripcionesCtrl.getListaSuscripciones
+);
+app.post("/api/info-paquetes", paquetesCtrl.getPaquetes);
+app.post("/api/info-descuentos", descuentoCtrl.getDescuento);
 
+app.get(
+  "/api/validarsuscriptor",
+  validacionSuscriptorCtrl.getValidacionSuscriptor
+);
+app.get(
+  "/api/lista-suscripciones",
+  listaSuscripcionesCtrl.getListaSuscripciones
+);
+app.get("/api/info-paquetes", paquetesCtrl.getPaquetes);
+app.get("/api/info-descuentos", descuentoCtrl.getDescuento);
 
-app.get('/api/validarsuscriptor',validacionSuscriptorCtrl.getValidacionSuscriptor);
-app.get('/api/lista-suscripciones',listaSuscripcionesCtrl.getListaSuscripciones);
-app.get('/api/info-paquetes',paquetesCtrl.getPaquetes);
-app.get('/api/info-descuentos',descuentoCtrl.getDescuento);
-
-
-
-
-
-app.listen(port,()=>{
-    console.log(`Api rest corriendo en http://localhost:${port}`);
-});;
+app.listen(port, () => {
+  console.log(`Api rest corriendo en http://localhost:${port}`);
+});
--- src/controllers/api_info_suscripciones.js
+++ src/controllers/api_info_suscripciones.js
@@ -1,133 +1,42 @@
-'use strict';
-const con = require('../conexion/conexion');
+"use strict";
+const con = require("../conexion/conexion");
 
+function getListaSuscripciones(req, res) {
+  let nro_document = req.body.nro_documento || req.query.nro_documento;
+  let tipo_document = req.body.tipo_documento || req.query.tipo_documento;
+  let id_subcategoria = req.body.subcategoria || req.query.subcategoria;
 
-function getListaSuscripciones(req,res){
+  let sql_suscripcion = "SELECT ";
+  sql_suscripcion += "sus.id_suscripcion, ";
+  sql_suscripcion += "sus.producto, ";
+  sql_suscripcion += "sus.periodo ";
+  sql_suscripcion += "FROM suscripcion sus ";
+  sql_suscripcion +=
+    "INNER JOIN cliente cli ON cli.id_cliente = sus.id_cliente ";
+  sql_suscripcion += "WHERE sus.id_subcategoria=" + id_subcategoria;
 
-    console.log(req.body);
-
-    let nro_document =  req.body.nro_documento;
-    let tipo_document = req.body.tipo_documento;
-    var id_subcategoria = req.body.subcategoria;
-
-
-    //let id_subcategory = parseInt(id_subcategoria);
-    //console.log(id_subcategory);
-    //console.log(typeof id_subcategoria);
-        //console.log(id_subcategoria);
-        
-
-        
-    if (tipo_document == 1) {
-
-            var sql_suscripcion = "SELECT ";
-            sql_suscripcion+="sus.id_suscripcion, ";
-            sql_suscripcion+="sus.producto, ";
-            sql_suscripcion+="sus.periodo ";
-            sql_suscripcion+="FROM suscripcion sus ";
-            sql_suscripcion+="INNER JOIN cliente cli ON cli.id_cliente = sus.id_cliente ";
-            sql_suscripcion+="WHERE sus.id_subcategoria="+id_subcategoria+" AND cli.nrodni="+nro_document;
-            
-            
-            con.query(sql_suscripcion, function (err, suscripciones, field) {
-                console.log(suscripciones.rows);
-                if(suscripciones.rowCount == 0){
-                    res.status(200).json({
-                        message: 'No Tiene suscripcion',
-                        code: '0'
-                    });
-            
-                }
-                
-                
-                if(suscripciones.rowCount > 0){
-                    res.status(200).json({
-                        message: 'tiene  suscripcion',
-                        code: '1'
-                    });
-            
-                }
-                
-            });
-            return false;
-    
-    } else if (tipo_document == 2) {
-
-            var sql_suscripcion = "SELECT ";
-            sql_suscripcion+="sus.id_suscripcion, ";
-            sql_suscripcion+="sus.producto, ";
-            sql_suscripcion+="sus.periodo ";
-            sql_suscripcion+="FROM suscripcion sus ";
-            sql_suscripcion+="INNER JOIN cliente cli ON cli.id_cliente = sus.id_cliente ";
-            sql_suscripcion+="WHERE sus.id_subcategoria=" +id_subcategoria+ " AND cli.nrocarnetextranjeria="+nro_document;
-            
-            
-            con.query(sql_suscripcion, function (err, suscripciones, field) {
-                console.log(suscripciones.rows);
-                if(suscripciones.rowCount == 0){
-                    res.status(200).json({
-                        message: 'No Tiene suscripcion',
-                        code: '0'
-                    });
-            
-                }
-
-                if(suscripciones.rowCount > 0){
-                    res.status(200).json({
-                        message: 'Tiene suscripcion',
-                        code: '1'
-                    });
-            
-                }
-            });
-
-        return false;
-
-
-    } else if (tipo_document == 3) {
-
-        var sql_suscripcion = "SELECT ";
-        sql_suscripcion+="sus.id_suscripcion, ";
-        sql_suscripcion+="sus.producto, ";
-        sql_suscripcion+="sus.periodo ";
-        sql_suscripcion+="FROM suscripcion sus ";
-        sql_suscripcion+="INNER JOIN cliente cli ON cli.id_cliente = sus.id_cliente ";
-        sql_suscripcion+="WHERE sus.id_subcategoria=" +id_subcategoria+ " AND cli.nroruc="+nro_document;
-
-            
-        con.query(sql_suscripcion, function (err, suscripciones, field) {
-                console.log(suscripciones.rows);
-                if(suscripciones.rowCount == 0){
-                    res.status(200).json({
-                        message: 'No Tiene suscripcion',
-                        code: '0'
-                    });
-            
-                }
-
-                if(suscripciones.rowCount > 0){
-                    res.status(200).json({
-                        message: 'tiene ninguna suscripcion',
-                        code: '1'
-                    });
-            
-                }
-            });
-            return false;
-
+  if (tipo_document == 1) {
+    sql_suscripcion += " AND cli.nrodni=" + nro_document;
+  } else if (tipo_document == 2) {
+    sql_suscripcion += " AND cli.nrocarnetextranjeria=" + nro_document;
+  } else if (tipo_document == 3) {
+    sql_suscripcion += " AND cli.nroruc=" + nro_document;
+  }
+  con.query(sql_suscripcion, function (err, suscripciones, field) {
+    if (suscripciones.rowCount == 0) {
+      res.status(200).json({
+        message: "No Tiene suscripcion",
+        code: "0",
+      });
     } else {
-        res.status(200).json({
-            message: 'El numero de documento identificado no es correcto'
-        });
+      res.status(200).json({
+        message: "tiene  suscripcion",
+        code: "1",
+      });
     }
-
-    
-
+  });
 }
-
 
 module.exports = {
-	getListaSuscripciones,
-}
-
-
+  getListaSuscripciones,
+};
