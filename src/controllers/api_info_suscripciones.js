1aa4dd7e53088c6bcd14e26a26c8ac7b1e0b030e
Martin Luque <martindeveloper4@gmail.com>
2020-12-11 09:04:22 -0500

    info paqueasdasdastkkdasdasdasddeasasdhhasddasd

--- src/controllers/api_info_suscripciones.js
+++ src/controllers/api_info_suscripciones.js
@@ -4,20 +4,21 @@ const con = require('../conexion/conexion');
 
 function getListaSuscripciones(req,res){
 
+    console.log(req.body);
 
-        console.log(req.body);
-        var id_subcategoria = req.body.subcategoria;
-        let nro_document =  req.body.nro_documento;
+    var id_subcategoria = req.body.subcategoria;
+    let nro_document =  req.body.nro_documento;
+    let tipo_document = req.body.tipo_documento;
 
 
-        //let id_subcategory = parseInt(id_subcategoria);
-        //console.log(id_subcategory);
-        //console.log(typeof id_subcategoria);
+    //let id_subcategory = parseInt(id_subcategoria);
+    //console.log(id_subcategory);
+    //console.log(typeof id_subcategoria);
         //console.log(id_subcategoria);
         
 
         
-        if (id_subcategoria == 1) {
+    if (tipo_document == 1) {
 
             var sql_suscripcion = "SELECT ";
             sql_suscripcion+="sus.id_suscripcion, ";
@@ -25,31 +26,32 @@ function getListaSuscripciones(req,res){
             sql_suscripcion+="sus.periodo ";
             sql_suscripcion+="FROM suscripcion sus ";
             sql_suscripcion+="INNER JOIN cliente cli ON cli.id_cliente = sus.id_cliente ";
-            sql_suscripcion+="WHERE sus.id_subcategoria=" +id_subcategoria+ " AND cli.nrodni="+nro_document;
+            sql_suscripcion+="WHERE sus.id_subcategoria="+id_subcategoria+" AND cli.nrodni="+nro_document;
             
             
             con.query(sql_suscripcion, function (err, suscripciones, field) {
                 console.log(suscripciones.rows);
-                if(suscripciones.rowCount > 0){
-                    res.status(200).json({
-                        message: 'Tiene suscripcion',
-                        code: '1'
-                    });
-            
-                }
-                
-                
                 if(suscripciones.rowCount == 0){
                     res.status(200).json({
-                        message: 'No tiene ninguna suscripcion',
+                        message: 'No Tiene suscripcion',
                         code: '0'
                     });
             
                 }
                 
+                
+                if(suscripciones.rowCount > 0){
+                    res.status(200).json({
+                        message: 'tiene  suscripcion',
+                        code: '1'
+                    });
+            
+                }
+                
             });
+            return false;
     
-    } else if (id_subcategoria == 2) {
+    } else if (tipo_document == 2) {
 
             var sql_suscripcion = "SELECT ";
             sql_suscripcion+="sus.id_suscripcion, ";
@@ -62,14 +64,6 @@ function getListaSuscripciones(req,res){
             
             con.query(sql_suscripcion, function (err, suscripciones, field) {
                 console.log(suscripciones.rows);
-                if(suscripciones.rowCount == 1){
-                    res.status(200).json({
-                        message: 'Tiene suscripcion',
-                        code: '1'
-                    });
-            
-                }
-
                 if(suscripciones.rowCount == 0){
                     res.status(200).json({
                         message: 'No Tiene suscripcion',
@@ -77,39 +71,49 @@ function getListaSuscripciones(req,res){
                     });
             
                 }
-            });
 
-
-    } else if (id_subcategoria == 3) {
-
-        var sql_suscripcion = "SELECT ";
-            sql_suscripcion+="sus.id_suscripcion, ";
-            sql_suscripcion+="sus.producto, ";
-            sql_suscripcion+="sus.periodo ";
-            sql_suscripcion+="FROM suscripcion sus ";
-            sql_suscripcion+="INNER JOIN cliente cli ON cli.id_cliente = sus.id_cliente ";
-            sql_suscripcion+="WHERE sus.id_subcategoria=" +id_subcategoria+ " AND cli.nroruc="+nro_document;
-
-            
-            con.query(sql_suscripcion, function (err, suscripciones, field) {
-                console.log(suscripciones.rows);
-                if(suscripciones.rowCount == 1){
+                if(suscripciones.rowCount > 0){
                     res.status(200).json({
                         message: 'Tiene suscripcion',
                         code: '1'
                     });
             
                 }
+            });
 
+        return false;
+
+
+    } else if (tipo_document == 3) {
+
+        var sql_suscripcion = "SELECT ";
+        sql_suscripcion+="sus.id_suscripcion, ";
+        sql_suscripcion+="sus.producto, ";
+        sql_suscripcion+="sus.periodo ";
+        sql_suscripcion+="FROM suscripcion sus ";
+        sql_suscripcion+="INNER JOIN cliente cli ON cli.id_cliente = sus.id_cliente ";
+        sql_suscripcion+="WHERE sus.id_subcategoria=" +id_subcategoria+ " AND cli.nroruc="+nro_document;
+
+            
+        con.query(sql_suscripcion, function (err, suscripciones, field) {
+                console.log(suscripciones.rows);
                 if(suscripciones.rowCount == 0){
                     res.status(200).json({
-                        message: 'No tiene ninguna suscripcion',
+                        message: 'No Tiene suscripcion',
                         code: '0'
                     });
             
                 }
-            });
 
+                if(suscripciones.rowCount > 0){
+                    res.status(200).json({
+                        message: 'tiene ninguna suscripcion',
+                        code: '1'
+                    });
+            
+                }
+            });
+            return false;
 
     } else {
         res.status(200).json({
