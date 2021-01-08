14648df593d3d830471195f679479dbed77fc2eb
majerhua123 <majerhua123@gmail.com>
2021-01-08 13:18:38 -0500

    s

--- src/conexion/conexion.js
+++ src/conexion/conexion.js
@@ -6,10 +6,10 @@ const { Pool } = require("pg");
 // Coloca aquí tus credenciales
 var pool = new Pool({
   user: "postgres",
-  password: "rpalatam20",
+  password: "IT0kYg53L56Mk3HYTZ4m",
   database: "AWS_ElComercio",
   port: 5432,
-  host: "database.cpehi2ylfzlh.us-east-1.rds.amazonaws.com",
+  host: "bpi-rpa-latam.cifz34qnrwkb.us-east-1.rds.amazonaws.com",
   ssl: { rejectUnauthorized: false },
 });
 
